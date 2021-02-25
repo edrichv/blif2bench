02532e7a45151ac5d824847e913e1038d0055e53
edrich <edrichverburg@gmail.com>
2021-02-25 09:55:12 +0200

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+import blifparse
+import toml
+
+config = toml.load('config.toml')
+files_conf = config['Files']
+in_blif = files_conf['work_dir'] + files_conf['in_blif']
+out_blif = in_blif.replace('.blif','.bench')
+
+blifparse.to_bench(in_blif,out_blif, config['BBGT'])
